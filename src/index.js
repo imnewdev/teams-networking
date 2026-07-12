e5e5ccc53873a2ee9938640682b644f9717c4ede
Laurentiu Matei <mateilaurentiu2012@gmail.com>
2026-07-12 18:57:18 +0300

    render teams

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./style.css";
 
+function getTeamAsHTML(team) {
+  return `<tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.Name}</td>
+        <td>${team.Url}</td>
+        <td>X</td>
+        </tr>`;
+}
+
+function renderTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+
+  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn("teams?", teams);
+      renderTeams(teams);
     });
 }
 
